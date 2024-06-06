e8b67909bf633b709d06d64884a31f2df7518a4a
jasonlin0301 <jasonlin119@gmail.com>
2024-06-06 11:45:40 +0800

    modify

--- 2024_06_06/index.py
+++ 2024_06_06/index.py
@@ -5,11 +5,14 @@ from ttkthemes import ThemedTk
 import tools
 from tkinter import messagebox
 from tkinter.simpledialog import Dialog
+from datetime import datetime
 
 class Window(ThemedTk):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
         self.title("全台空氣品質指標(AQI)")
+        #self.option_add("*Font","微軟正黑體 40")
+        #定義style的名稱
         style = ttk.Style()
         style.configure('Top.TFrame')
         style.configure('Top.TLabel',font=('Helvetica',25,'bold'))
@@ -24,20 +27,32 @@ class Window(ThemedTk):
         ttk.Button(func_frame,text="pm2.5品質最好的5個",command=self.click3).pack(side='left',expand=True)
         ttk.Button(func_frame,text="pm2.5品質最好的5個",command=self.click4).pack(side='left',expand=True)
         func_frame.pack(ipadx=100,ipady=30,padx=10,pady=10)
-
-    def download_parse_data(self) ->list[dict] | None:
+    
+    def download_parse_data(self)->list[dict] | None:
         try:
-            all_data:dict[any] = tools.download_json()
+            all_data:dict[any] = tools.download_json()            
         except Exception as error:
             messagebox.showwarning("出現錯誤","出現小錯誤,請稍後再試!")
             return
         else:        
             data:list[dict] = tools.get_data(all_data)
             return data
-    
+                  
+
     def click1(self):
-        data:list[dict] = self.download_parse_data()
-        print(data)
+        if (tools.AQI.aqi_records is None) or (tools.AQI.update_time is None):
+            tools.AQI.aqi_records = self.download_parse_data()
+            tools.AQI.update_time = datetime.now()
+        elif((datetime.now()-tools.AQI.update_time).seconds >= 60 * 60):
+            tools.AQI.aqi_records = self.download_parse_data()
+            tools.AQI.update_time = datetime.now()       
+
+        data:list[dict] = tools.AQI.aqi_records
+        sorted_data:list[dict] = sorted(data,key=lambda value:value['aqi'])
+        best_aqi:list[dict] = sorted_data[:5]
+        print(best_aqi)
+              
+            
     
     def click2(self):
         messagebox.showerror("Error","Error message")
@@ -53,10 +68,10 @@ class ShowInfo(Dialog):
         super().__init__(**kwargs)
     
     def body(self, master):
-        text = tk.Text(self,height=8)
+        text = tk.Text(self,height=8,font=('Helvetica',25),width=40)
         text.pack(padx=10,pady=10)
-        text.insert(tk.INSERT,"測試文字")
-        text.config(state='disabled')  
+        text.insert(tk.INSERT,"測試的文字")
+        text.config(state='disabled')
         return None
 
 
