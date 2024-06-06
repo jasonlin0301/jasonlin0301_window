1494a40005e3e0ff7d069a2205ae98d8cbc0f1c3
jasonlin0301 <jasonlin119@gmail.com>
2024-06-06 16:09:22 +0800

    modify

--- 2024_06_06/index.py
+++ 2024_06_06/index.py
@@ -1,6 +1,6 @@
 from pprint import pprint
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk,Misc,Frame,Event
 from ttkthemes import ThemedTk
 import tools
 from tkinter import messagebox
@@ -25,7 +25,7 @@ class Window(ThemedTk):
         ttk.Button(func_frame,text="AQI品質最好的5個",command=self.click1).pack(side='left',expand=True)
         ttk.Button(func_frame,text="AQI品質最差的5個",command=self.click2).pack(side='left',expand=True)
         ttk.Button(func_frame,text="pm2.5品質最好的5個",command=self.click3).pack(side='left',expand=True)
-        ttk.Button(func_frame,text="pm2.5品質最好的5個",command=self.click4).pack(side='left',expand=True)
+        ttk.Button(func_frame,text="pm2.5品質最差的5個",command=self.click4).pack(side='left',expand=True)
         func_frame.pack(ipadx=100,ipady=30,padx=10,pady=10)
     
     def download_parse_data(self)->list[dict] | None:
@@ -52,33 +52,84 @@ class Window(ThemedTk):
         data:list[dict] = tools.AQI.aqi_records
         sorted_data:list[dict] = sorted(data,key=lambda value:value['aqi'])
         best_aqi:list[dict] = sorted_data[:5]
-        print(best_aqi)
+        def abc(value:dict)->str:
+            return f"{value['county']} - {value['site_name']} - aqi:{value['aqi']} - 狀態:{value['status']} - {value['date']}"
+        message_data:list[str] = list(map(abc,best_aqi))
+        message = "\n".join(message_data)
+        print(message)
+        ShowInfo(parent=self,title="全台aqi最佳前5個區域",message=message)
               
             
     
     def click2(self):
         self.update_data()
-        messagebox.showerror("Error","Error message")
+        data:list[dict] = tools.AQI.aqi_records
+        sorted_data:list[dict] = sorted(data,key=lambda value:value['aqi'],reverse=True)
+        best_aqi:list[dict] = sorted_data[:5]
+        def abc(value:dict)->str:
+            return f"{value['county']} - {value['site_name']} - aqi:{value['aqi']} - 狀態:{value['status']} - {value['date']}"
+        message_data:list[str] = list(map(abc,best_aqi))
+        message = "\n".join(message_data)
+        print(message)
+        ShowInfo(parent=self,title="全台aqi最差5個區域",message=message)
+        
 
     def click3(self):
         self.update_data()
-        messagebox.showwarning("Warning","Warning message")
+        data:list[dict] = tools.AQI.aqi_records
+        sorted_data:list[dict] = sorted(data,key=lambda value:value['pm25'])
+        best_aqi:list[dict] = sorted_data[:5]
+        def abc(value:dict)->str:
+            return f"{value['county']} - {value['site_name']} - pm2.5:{value['pm25']} - 狀態:{value['status']} - {value['date']}"
+        message_data:list[str] = list(map(abc,best_aqi))
+        message = "\n".join(message_data)
+        print(message)
+        ShowInfo(parent=self,title="全台pm2.5最佳前5個區域",message=message)
     
-    def click4(self):
+    def click4(self):        
         self.update_data()
-        ShowInfo(parent=self,title="這是Dialog")
+        data:list[dict] = tools.AQI.aqi_records
+        sorted_data:list[dict] = sorted(data,key=lambda value:value['pm25'],reverse=True)
+        best_aqi:list[dict] = sorted_data[:5]
+        def abc(value:dict)->str:
+            return f"{value['county']} - {value['site_name']} - pm2.5:{value['pm25']} - 狀態:{value['status']} - {value['date']}"
+        message_data:list[str] = list(map(abc,best_aqi))
+        message = "\n".join(message_data)
+        print(message)
+        ShowInfo(parent=self,title="全台pm2.5最差5個區域",message=message)
 
 class ShowInfo(Dialog):
-    def __init__(self,**kwargs):
-        super().__init__(**kwargs)
+    def __init__(self,parent:Misc,title:str|None = None,message:str=""):
+        self.message = message 
+        super().__init__(parent=parent,title=title) #最後才執行       
+
     
-    def body(self, master):
-        text = tk.Text(self,height=8,font=('Helvetica',25),width=40)
+    def body(self, master: Frame) -> Misc | None:
+        text = tk.Text(self,height=8,font=('Helvetica',15),width=60)
         text.pack(padx=10,pady=10)
-        text.insert(tk.INSERT,"測試的文字")
+        text.insert(tk.INSERT,self.message)
         text.config(state='disabled')
         return None
+    
+    def apply(self) -> None:
+        '''
+        使用者按下內建的ok button,會執行的內容
+        '''
+        print("使用者按下ok了")
 
+    def buttonbox(self) -> None:
+        '''
+        自訂button
+        '''
+        box = tk.Frame(self)
+        self.ok_button = tk.Button(box, text="確定", width=10, command=self.ok)
+        self.ok_button.pack(pady=(20,30),ipady=10)
+        box.pack()
+
+    def ok(self) -> None:
+        print("OK button was clicked!")       
+        super().ok()
+    
 
 
 def main():
