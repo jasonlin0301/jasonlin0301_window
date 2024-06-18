7a9411d85c5aa2c010edf6809627cd547242c67b
jasonlin0301 <jasonlin119@gmail.com>
2024-06-18 08:44:51 +0800

    modify

--- 2024_06_06/tools.py
+++ 2024_06_06/tools.py
@@ -1,6 +1,7 @@
 import requests
 from  requests import JSONDecodeError
 from pydantic import BaseModel,RootModel,Field,field_validator
+from datetime import datetime
 
 class Site(BaseModel):
     site_name:str = Field(alias='sitename')
@@ -17,7 +18,6 @@ class Site(BaseModel):
             return "0.0"
         else:
             return value
-
 
 class Records(RootModel):
     root:list[Site]
@@ -37,7 +37,16 @@ def download_json()->dict[any]:
                 raise Exception("api_key為測試用,連線已至上限,請稍後再試")
         else:
             raise Exception("下載狀態碼不是200")
+        
 def get_data(all_data:dict[any]) -> list[dict]:
-        records:Records = Records.model_validate(all_data['records'])
-        data:list[dict] = records.model_dump()
-        return (data)
+    records:Records = Records.model_validate(all_data['records'])
+    data:list[dict] = records.model_dump()
+    return data
+
+class AQI(object):
+    '''
+    利用class attribute aqi_records儲存下載資料
+    利用class attribute update_time儲存下載時間
+    '''
+    aqi_records:list[dict] | None = None
+    update_time:datetime | None = None
