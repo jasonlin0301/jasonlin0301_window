cc27c68031f0520f167192ab39f5b7a393514b28
jasonlin0301 <jasonlin119@gmail.com>
2024-08-01 14:54:22 +0800

    modify

--- dash_flask/main.py
+++ dash_flask/main.py
@@ -5,6 +5,7 @@ from dashboard.board1 import app1
 from dashboard.board2 import app2
 from dashboard.board3 import app3
 
+
 app = Flask(__name__)
 
 # 使用 DispatcherMiddleware 將多個 Dash 應用與 Flask 應用結合
@@ -16,18 +17,15 @@ application = DispatcherMiddleware(app, {
 
 @app.route("/")
 def index():
-    # 渲染靜態首頁
     return render_template("index.html")
 
 @app.route("/dashboard/app2")
 def dashboard_app2():
-    # 渲染另外一個靜態頁面，如果需要的話
     return render_template("index.html.jinja")
 
 @app.route("/dashboard/app3")
-def dashboard_app2():
-    # 渲染另外一個靜態頁面，如果需要的話
+def dashboard_app3():
     return render_template("index1.html.jinja")
 
-if __name__ == "__main__":
-    run_simple("localhost", 8080, application, use_debugger=True, use_reloader=True)
+if __name__ == '__main__':
+    run_simple("localhost", 8056, application, use_debugger=True, use_reloader=True)
