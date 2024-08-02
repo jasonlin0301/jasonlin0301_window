a3364a0fa32f0e5d65ff14891ba5ea2883e1c1eb
jasonlin0301 <jasonlin119@gmail.com>
2024-08-02 08:22:35 +0800

    modify

--- dash_flask/dashboard/board1.py
+++ dash_flask/dashboard/board1.py
@@ -1,23 +1,16 @@
-import os
-import dash
 import pandas as pd
-import plotly.express as px
-from dash import Dash, html, dcc, Input, Output, callback
+from dash import Dash, html
 from sqlalchemy import create_engine
 
 # 初始化 Dash 應用
 app1 = Dash(__name__, requests_pathname_prefix='/dashboard/app1/')
 app1.title = '臺灣太陽能系統評估表單'
 
-# 確定目錄設置正確，使用相對路徑設置文件路徑
-current_dir = os.getcwd()
-target_dir = os.path.join(current_dir, 'dash_flask')
-
 # 設置資料庫連線
 db_url = 'postgresql://tvdi_postgresql_etik_user:4jYKNZqoOCkdoHsQIdHBOiL27yixeBTM@dpg-cqhf92aju9rs738kbi8g-a.singapore-postgres.render.com/tvdi_postgresql_etik_o8g3'
 engine = create_engine(db_url)
 
-# 假設您的表格名稱是 dash_web，以及要選取的欄位
+# 假設您的表格名稱是 dash_web
 query = "SELECT * FROM dash_web"
 
 # 使用 Pandas 讀取資料
@@ -26,29 +19,20 @@ data = pd.read_sql(query, engine)
 # 關閉資料庫連線
 engine.dispose()
 
-# 顯示 DataFrame 的欄位名稱
-print("DataFrame columns:", data.columns)
-
-# 去除欄位名稱中的空白字符
-data.columns = data.columns.str.strip()
-print("Cleaned DataFrame columns:", data.columns)
-
+# 設置 Dash 應用的佈局
 app1.layout = html.Div([
-    dcc.Dropdown(
-       options = year,
-       value = year[0],
-       id='year'
-    ),
-    html.Hr(),
-    dash_table.DataTable(id='sites_table')
+    html.H1('所有資料表格'),
+    html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in data.columns])
+        ),
+        html.Tbody([
+            html.Tr([html.Td(data.iloc[i][col]) for col in data.columns])
+            for i in range(len(data))
+        ])
+    ])
 ])
-# #如果要連結2個dash,必需要加上app1
-# @app1.callback(
-#     Output('sites_table','data'),
-#     Input('year','value')
-# )
-# def selected_area(year_value):
-#     content = data.get_snaOfArea(area=year_value)
-#     df = pd.DataFrame(content)
-#     df.columns = ['站點名稱','總數','可借','可還','日期','狀態']
-#     return df.to_dict('records')
+
+# 啟動 Dash 應用
+if __name__ == '__main__':
+    app1.run_server(debug=True)
